3cd15822f2394fc9fefe4d7599348022794bba7c
Brandon Salmon <brandonsalmon22@gmail.com>
2015-12-06 21:13:16 -0800

    added window managers for runnable programs

---
+++ rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/lift_object_window_manager.py
@@ -0,0 +1,35 @@
+import os
+import rospy
+import rospkg
+
+from python_qt_binding import loadUi
+from python_qt_binding.QtGui import QWidget
+
+from .window_types import WindowTypes
+
+class LiftObjectWindowManager:
+
+  def __init__(self, pr2_interface):
+    self._pr2_interface = pr2_interface
+
+    # Create QWidget object (this is object that represents the window
+    # that the user actually sees).
+    self._widget = QWidget()
+
+    # Get path to UI file which should be in the "resource" folder of this package
+    ui_file = os.path.join(
+        rospkg.RosPack().get_path(
+            'rqt_pr2_hand_syntouch_sensor_interface'), 'resource', 
+            'liftobject.ui')
+
+    # Extend the widget with all attributes and children from UI file
+    loadUi(ui_file, self._widget)
+
+    # Give QObjects reasonable names
+    self._widget.setObjectName('LiftObject')
+
+    self._widget.setWindowTitle('Lift Object Using PR2 Arm')
+
+    # Add widget to the user interface
+    user_interface = pr2_interface.get_user_interface()
+    user_interface.add_widget(self._widget)
--- rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/window_creation_manager.py
+++ rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/window_creation_manager.py
@@ -5,6 +5,12 @@ from .robot_window_manager import RobotWindowManager
 from .run_programs_window_manager import RunProgramsWindowManager
 from .sensor_window_manager import SensorWindowManager
 from .stats_window_manager import LifetimeStatsWindowManager
+from .pulse_analysis_window_manager import PulseAnalysisWindowManager
+from .lift_object_window_manager import LiftObjectWindowManager
+from .place_object_window_manager import PlaceObjectWindowManager
+#from .rotate_object_window_manager import RotateObjectWindowManager
+from .switch_hands_window_manager import SwitchHandsWindowManager
+
 
 from .window_types import WindowTypes
 
@@ -24,22 +30,51 @@ class WindowCreationManager:
       self._open_window_managers.add(IndexWindowManager(self._pr2_interface))
       self._open_window_types.add(window_type)
     elif window_type is WindowTypes.ConnectionWindow:
-      self._open_window_managers.add(ConnectionWindowManager(self._pr2_interface))
+      self._open_window_managers.add(
+          ConnectionWindowManager(self._pr2_interface))
       self._open_window_types.add(window_type)
     elif window_type is WindowTypes.SensorVisualizerWindow:
       self._open_window_managers.add(SensorWindowManager(self._pr2_interface))
       self._open_window_types.add(window_type)
-    elif window_type is WindowTypes.RunProgramsWindow:
-      self._open_window_managers.add(RunProgramsWindowManager(self._pr2_interface))
     elif window_type is WindowTypes.RobotVisualizerWindow:
       self._open_window_managers.add(RobotWindowManager(self._pr2_interface))
       self._open_window_types.add(window_type)
     elif window_type is WindowTypes.LifetimeStatsWindow:
-      self._open_window_managers.add(LifetimeStatsWindowManager(self._pr2_interface))
+      self._open_window_managers.add(
+          LifetimeStatsWindowManager(self._pr2_interface))
       self._open_window_types.add(window_type)
     elif window_type is WindowTypes.DataGraphsWindow:
-      self._open_window_managers.add(DataGraphsWindowManager(self._pr2_interface))
+      self._open_window_managers.add(
+          DataGraphsWindowManager(self._pr2_interface))
       self._open_window_types.add(window_type)
+    elif window_type is WindowTypes.RunProgramsWindow:
+      self._open_window_managers.add(
+          RunProgramsWindowManager(self._pr2_interface))
+      self._open_window_types.add(window_type)
+    # Below are windows that can be opened from the "Run Programs Window", which
+    # has its own windows.
+    elif window_type is WindowTypes.PulseAnalysisWindow:
+      self._open_window_managers.add(
+          PulseAnalysisWindowManager(self._pr2_interface))
+      self._open_window_types.add(window_type)
+    elif window_type is WindowTypes.LiftObjectWindow:
+      self._open_window_managers.add(
+          LiftObjectWindowManager(self._pr2_interface))
+      self._open_window_types.add(window_type)
+    elif window_type is WindowTypes.PlaceObjectWindow:
+      self._open_window_managers.add(
+          PlaceObjectWindowManager(self._pr2_interface))
+      self._open_window_types.add(window_type)
+    elif window_type is WindowTypes.RotateObjectWindow:
+      raise NotImplementedError
+#      self._open_window_managers.add(
+#          RotateObjectWindowManager(self._pr2_interface))
+#      self._open_window_types.add(window_type)
+    elif window_type is WindowTypes.SwitchHandsWindow:
+      self._open_window_managers.add(
+          SwitchHandsWindowManager(self._pr2_interface))
+      self._open_window_types.add(window_type)
+     
     else:
       raise NotImplementedError
     
