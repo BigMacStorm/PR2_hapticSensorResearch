2e9044a05d66c7d48f1e9d7d43114f4164058290
Edward Miles <elmiles93@live.com>
2015-12-08 01:17:34 -0800

    I added comments to the stats_window_manager.py

--- rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/stats_window_manager.py
+++ rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/stats_window_manager.py
@@ -10,6 +10,9 @@ from .window_manager import WindowManager
 
 class LifetimeStatsWindowManager(WindowManager):
 
+  # This function will initialize the window and all widgets attached to this window.
+  # Args:
+  #	pr2_interface: the single pr2 interface plug in object
   def __init__(self, pr2_interface):
     # Initialize the WindowManager base class. The WindowManager class
     # creates the _widget object that will be used by this window and
@@ -30,7 +33,6 @@ class LifetimeStatsWindowManager(WindowManager):
 
     # Give QObjects reasonable names
     self._widget.setObjectName('LifetimeStatsWindow')
-
     self._widget.setWindowTitle('Lifetime Statistics')
 
     # Temporary workaround: Don't open up the window yet here, we just need
@@ -42,14 +44,28 @@ class LifetimeStatsWindowManager(WindowManager):
     # user_interface = pr2_interface.get_user_interface()
     # user_interface.add_widget(self._widget)
 
+    # Create a thread that will begin tracking the lifetime statistics
     self._worker = threading.Thread(target=self.track_lifetime_stats)
     self._worker.start()
 
+  # This function will be called be a thread to keep track of the lifetime statistics
+  # of the PR2. This function will also update the lifetime statistics window
   def track_lifetime_stats(self):
+    # Initialize local variables.
     rate = rospy.Rate(5) # 5hz
     last_data_point = None
+
+    # While rospy is still running and this window has not been destroyed.
     while not rospy.is_shutdown() and not self._destroyed:
+      # Get the most recent data from the pr2 interface.
       current_data_point = self._pr2_interface.get_most_recent_data()
+
+      # If the newest data recieved is equal to the last set of data recieved
+      # then the pr2 is not "connected". Else the newest data is not equal to the 
+      # last set of data recieved and the pr2 is "connected" and the functions updates
+      # the labels of the window and the last connected time. This is currently
+      # just for testing purposes to use with the mock_pr2.py which is simulating
+      # incoming pr2 data.
       if last_data_point == current_data_point or not last_data_point:
         self._disconnected = True
       else:
@@ -60,6 +76,8 @@ class LifetimeStatsWindowManager(WindowManager):
       rate.sleep()
       last_data_point = current_data_point
 
+  # This function will run through all of the lables on the Lifetime statistics window
+  # and update them
   def update_labels(self):
     self.update_pr2_interface_uptime_label()
     self.update_connection_uptime_label()
@@ -67,11 +85,17 @@ class LifetimeStatsWindowManager(WindowManager):
     self.update_average_sample_rate_label()
     self.update_actions_performed_label()
     self.update_most_performed_action_label()
-    
+ 
+  # This function is used to update the data samples label specifically because it has to 
+  # keep track of the total amount of data samples recieved over the lifetime of the
+  # interface of the PR2 
   def update_data_samples_label(self):
     total_data_samples = self._pr2_interface.count_data_time_ticks()
     self._widget.DataSamplesLabel.setText('%d' % total_data_samples)
 
+  # This function is used to update the average data samples rate label specifically
+  # because it  has to keep track of the average rate  of data samples recieved over
+  # the lifetime of the interface of the PR2 
   def update_average_sample_rate_label(self):
     total_data_samples = self._pr2_interface.count_data_time_ticks()
 
@@ -90,13 +114,24 @@ class LifetimeStatsWindowManager(WindowManager):
 
     self._widget.AverageSampleRateLabel.setText('%d hz' % data_samples_per_sec)
 
+  # This function is used to update the actions performed label specifically because
+  # it has to add up the total number of actions performed by the PR2
+  # NOTE: This is not implemented right now because the PR2 is not currently connected 
+  #       to our interface
   def update_actions_performed_label(self):
     pass
 
+  # This function is used to update the most performed action label specifically because
+  # it has to calculate which action was performed the most
+  # NOTE: This is not implemented right now because the PR2 is not currently connected
+  #       to our interface
   def update_most_performed_action_label(self):
     pass
 
+  # This function is used to update the interface uptime label specifically because it
+  # has to calculate the total amount of time that the interface has ran over its lifetime.
   def update_pr2_interface_uptime_label(self):
+    # get the current time
     t_now = rospy.get_rostime().to_nsec()
 
     # Get the pr2 interface uptime in nanoseconds.
@@ -113,7 +148,11 @@ class LifetimeStatsWindowManager(WindowManager):
     self._widget.PR2UptimeLabel.setText('%02d:%02d:%02d' 
         % (hours, minutes, seconds)) 
 
+  # This function is used to update the connection uptime label specifically because
+  # it has to calculate the total amount of time that the PR2 has been connected to
+  # the interface over its lifetime.
   def update_connection_uptime_label(self):
+    # get the current time
     t_now = rospy.get_rostime().to_nsec()
     
     # Get the connection uptime in nanoseconds, and also
@@ -136,6 +175,7 @@ class LifetimeStatsWindowManager(WindowManager):
     self._widget.ConnectionUptimeLabel.setText('%02d:%02d:%02d %s' 
         % (hours, minutes, seconds, disconnected_string)) 
 
+  # This function will reopen this window.
   def reopen(self):
     user_interface = self._pr2_interface.get_user_interface()
     user_interface.add_widget(self._widget)
