3ba1bba4b824ce536e8f05c3543b03279103b77d
Brandon Salmon <brandonsalmon22@gmail.com>
2015-12-08 01:47:02 -0800

    added interactivity to lift_hand

--- rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/lift_object_window_manager.py
+++ rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/lift_object_window_manager.py
@@ -36,6 +36,30 @@ class LiftObjectWindowManager(WindowManager):
     user_interface = pr2_interface.get_user_interface()
     user_interface.add_widget(self._widget)
 
+    # Register a listener for the lift button.
+    self._widget.LiftButton.clicked.connect(self._handle_lift_button_clicked)
+
+  # function to handle the lift button being clicked.
+  def _handle_lift_button_clicked(self):
+    try:
+      # Obtain the coordinates of the object to lift (relative to the PR2).
+      # Data comes from textboxes on the interface.
+      object_x_coordinate = float(self._widget.XTextBox.toPlainText())
+      object_y_coordinate = float(self._widget.YTextBox.toPlainText())
+      object_z_coordinate = float(self._widget.ZTextBox.toPlainText())
+      hand_to_use = self._widget.ComboBox.currentText()
+
+      self._widget.OutputTextBox.document().setPlainText(
+          'Attempting to lift object at coordinate (%s, %s, %s) with %s'
+          % (object_x_coordinate, object_y_coordinate, object_z_coordinate,
+             hand_to_use))
+    except ValueError:
+      self._widget.OutputTextBox.document().setPlainText(
+          'Parameters must be floats.')
+
+    # TODO: Add code to call the PR2_Controller to move the hand, control the
+    # hand to lift the object, etc.
+
   # calls the function to readd the widgets if the window was closed
   def reopen(self):
     user_interface = self._pr2_interface.get_user_interface()
