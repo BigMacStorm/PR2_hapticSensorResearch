7605c54bdd425a7c73f7677cfa4150bdd6f5b6e2
Edward Miles <elmiles93@live.com>
2015-12-08 01:39:23 -0800

    I added comments to window_manager.py

--- rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/window_manager.py
+++ rqt_pr2_hand_syntouch_sensor_interface/src/rqt_pr2_hand_syntouch_sensor_interface/window_manager.py
@@ -7,19 +7,19 @@ from python_qt_binding.QtGui import QWidget
 class WindowManager:
   __metaclass__ = abc.ABCMeta
 
-  @abc.abstractmethod
-  def __init__(self, pr2_interface):
   # Initialize the WindowManager base class. The WindowManager class
   # creates the _widget object that will be used by all windoww and
   # guarantees successful shutdown of rqt upon program termination.
   # Args:
   # 	pr2_interface: the single pr2 interface plug in object
+  @abc.abstractmethod
+  def __init__(self, pr2_interface):
     self._widget = QWidget()
     self._pr2_interface = pr2_interface
     self._destroyed = False
     self._widget.destroyed.connect(self.shutdown)
 
+  # This function signals to an individual window manager that it should start shutting down its threads and begin the process of closing itself
   def shutdown(self):
-  # Signals to an individual window manager that it should start shutting down its threads and begin the process of closing itself
     self._destroyed = True
 
